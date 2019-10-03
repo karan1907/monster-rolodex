2b3b5676c3988720f1b1159127c200b557c085fd
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-10-03 07:24:48 +0530

    Updates

---
+++ precache-manifest.5f49b84a929472c65532fd083bbb0ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d8aa39ac8690d94945f6c64bc05603",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "83fe36d8d623e2055915",
+    "url": "/monster-rolodex/static/css/main.7d673f27.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monster-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "83fe36d8d623e2055915",
+    "url": "/monster-rolodex/static/js/main.87e7c110.chunk.js"
+  },
+  {
+    "revision": "e4a84cd1159e6b489641",
+    "url": "/monster-rolodex/static/js/runtime-main.74ebc4ac.js"
+  }
+]);
